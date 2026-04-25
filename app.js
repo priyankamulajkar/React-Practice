106d4e1ca997b3f10d47847dc65e016cacededd0
Priyanka Kashinath Mulajkar <pmulajkar@gmail.com>
2026-04-25 23:51:33 +0530

    JSX and component creation

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const paragraph = React.createElement('p', {}, "This is paragraph tag");
-const parent = React.createElement('div', {id: 'parent1'},
-    React.createElement('div', {id:'parent2'},
-        [
-            React.createElement('h1', {id:'heading7'}, "I'm in Heading tag testing"),
-            React.createElement('h2', {id:'heading9'}, "I am in a Heading 2 tag"),
-        ]
-    )
-)
-console.log("parent", parent);
-console.log('paragraph', paragraph);
+const heading = React.createElement("h1", {}, "This is my Heading");
+console.log("Heading", heading);
 
-const rootReact = ReactDOM.createRoot(document.getElementById('root'));
-rootReact.render(parent);
-
-
-const parents = React.createElement("div", {id:"parent3"},
-    React.createElement('div', {id:"parent4"},
-        [
-            React.createElement("h1", {id:"heading8"}, "Heading1 tag 8"),
-            React.createElement('h2', {id:"heading8"}, "I a in a heading2 8th id tag")
-        ]
-
-    )
-
+const heading2 = (
+<>
+<h2>This is my heading 2 with react element</h2>
+</>
 );
-console.log("parents", parents);
 
-const rootReact2 = ReactDOM.createRoot(document.getElementById('root2'));
-rootReact2.render(parents);
+const number =  100;
 
+const HeadingComponent = () =>{
+    return(
+        <>
+            { heading2 }
+            { number /10 }
+             <h2>This is my functional component</h2>
+            <h3>This is my heading 3</h3>
+        </>
+    );
+}
 
+const AnotherComponent = () => (
+    <>
+        <HeadingComponent />
+        <HeadingComponent ></HeadingComponent>
+        <h1>we are testing this functional componet</h1>
+        {HeadingComponent()}
+        <h2>This is Another component</h2>
+        <h2>This is Another component</h2>
+        <h2>This is Another component</h2>
+    </>
+    
+)
+const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
+reactRoot.render(<AnotherComponent />);
